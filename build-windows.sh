0538abad6baa10be28102812e945cdf683f7fb5d
rojo-jojo <40956690+rojo-jojo@users.noreply.github.com>
2022-11-28 23:41:25 +0530

    still backprop

--- build-windows.sh
+++ build-windows.sh
@@ -2,5 +2,5 @@ curl -o cifar-10-python.tar.gz https://www.cs.toronto.edu/~kriz/cifar-10-python.
 mkdir cifar-data
 tar -xzvf cifar-10-python.tar.gz -C cifar-data
 python -m venv venv
-.\env\Scripts\activate
+.\venv\Scripts\activate
 pip install -r requirements.txt
