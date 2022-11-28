ca2ba9c18e8444ee1466db9e7b2ccb6d1a2db9ce
rojo-jojo <40956690+rojo-jojo@users.noreply.github.com>
2022-11-28 23:22:13 +0530

    stuck at backprop

--- build-windows.sh
+++ build-windows.sh
@@ -1,6 +1,6 @@
 curl -o cifar-10-python.tar.gz https://www.cs.toronto.edu/~kriz/cifar-10-python.tar.gz
 mkdir cifar-data
 tar -xzvf cifar-10-python.tar.gz -C cifar-data
-python3 -m venv venv
-. venv/bin/activate
+python -m venv venv
+.\env\Scripts\activate
 pip install -r requirements.txt
